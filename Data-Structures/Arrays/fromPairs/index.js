bd3480e83b06ae3382eb82308105ef037d38731b
John Lopez <john.lopez3695@gmail.com>
2024-02-29 08:59:30 -0500

    O(N)

---
+++ Data-Structures/Arrays/fromPairs/index.js
@@ -0,0 +1,24 @@
+/*-----------------------------------------------------------------
+Challenge: 14-fromPairs
+Difficulty: Intermediate
+Prompt:
+- Write a function named fromPairs that creates an object from an array containing nested arrays.
+- Each nested array will have two elements representing key/value pairs used to create key/value pairs in an object to be returned by the function.
+- If a key appears in multiple pairs, the rightmost pair should overwrite previous the previous entry in the object.
+Examples:
+fromPairs([ ['a', 1], ['b', 2], ['c', 3] ]) //=> { a: 1, b: 2, c: 3 }
+fromPairs([ ['name', 'Sam"], ['age', 24], ['name', 'Sally'] ]) //=> { name: "Sally", age: 24 }
+-----------------------------------------------------------------*/
+// Your solution for 14-fromPairs here:
+
+function fromPairs(arr) {
+  const obj = Object.fromEntries(arr);
+  return obj;
+}
+console.log(
+  fromPairs([
+    ["a", 1],
+    ["b", 2],
+    ["c", 3],
+  ])
+);
