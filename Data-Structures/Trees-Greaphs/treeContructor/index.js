bb34831423ed92ae0379fbe9c27e24d058fd37d8
John Lopez <john.lopez3695@gmail.com>
2024-03-27 11:50:32 -0400

    Tree constuctor for BST

---
+++ Data-Structures/Trees-Greaphs/treeContructor/index.js
@@ -0,0 +1,56 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+  insert(val) {
+    const newNode = new Node(val);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+
+      while (true) {
+        if (val === current.val) {
+          return undefined;
+        }
+        if (val < current.val) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (val > current.val) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
+  find() {}
+}
+
+const tree = new BST();
+tree.insert(10);
+tree.insert(13);
+tree.insert(11);
+tree.insert(9);
+tree.insert(3);
+tree.insert(12);
+tree.insert(34);
+tree.insert(2);
+tree.insert(7);
+console.log(tree);
